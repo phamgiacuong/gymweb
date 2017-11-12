045581c5e2588c8426d7b0608cfaf2ce3769b2b6
hoanganh <hoanganhA91013@gmail.com>
2017-11-12 17:44:44 +0700

    Add jquery-ujs turbolinks in application.js

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,9 +10,10 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-//= require jquery3
+//= require jquery
+//= require jquery_ujs
+//= require jquery-ui
 //= require popper
 //= require bootstrap-sprockets
-//= require jquery-ui
 //= require turbolinks
 //= require_tree .
